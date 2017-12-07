c1f2beb9609ae1d7f0bd809fb50514f0093a0c00
Muslyansah <muslyansah@gmail.com>
2017-12-07 14:19:02 +0700

    added

---
+++ masuk.php
@@ -0,0 +1,26 @@
+<?php $title = "Login"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<!-- head -->
+	<?php require_once 'layouts/partials/head.php'; ?>
+</head>
+<body>
+	<!-- navbar -->
+	<?php require_once 'layouts/partials/navbar.php'; ?>
+	
+	<!-- container -->
+	<div class="container isi-konten">
+		<!-- slideshow -->
+		<?php require_once 'layouts/partials/form-masuk.php'; ?>
+	</div>
+
+	<!-- footer -->
+	<?php require_once 'layouts/partials/footer.html'; ?>
+
+	<!-- js -->
+	<?php require_once 'layouts/partials/js-init.html'; ?>
+</body>
+</html>
